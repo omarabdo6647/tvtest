86cf06a4e8123f81cfc6fe97e02bba2053b8e3cf
unknown <cp3170@gmail.com>
2021-02-20 13:32:08 +0300

    create test case for deleteChannel and countChannels

--- tvchannels.test.js
+++ tvchannels.test.js
@@ -29,3 +29,45 @@ test('Test addChannel Function, add exist channel', () => {
 
 })
 
+//==========================================================================================
+// deleteChannel
+//==========================================================================================
+
+test('Test deleteChannel Function, delete unexist channel', () => {
+    let check = true
+    try {
+        manager.deleteChannel(546221655)
+    } catch {
+        check = false
+    }
+    expect(check)
+        .toBe(false)
+
+})
+test('Test deleteChannel Function, delete exist channel', () => {
+    let check = true
+    try {
+        manager.deleteChannel(5)
+    } catch {
+        check = false
+    }
+    expect(check)
+        .toBe(true)
+
+})
+
+//==========================================================================================
+// countChannels
+//==========================================================================================
+test('Test countChannels Function', () => {
+    let manager2 = new ChannelManager()
+    manager2.addChannel(new TVChannel(1, 'M+', 1, 'Music'))
+    manager2.addChannel(new TVChannel(2, 'Zee Aflam', 7, 'Movies'))
+    manager2.addChannel(new TVChannel(3, 'Russia Today', 5, 'News'))
+    manager2.addChannel(new TVChannel(4, 'CBC', 3, 'News'))
+    manager2.addChannel(new TVChannel(5, 'Bein', 9, 'Sports'))
+    let count = manager2.countChannels()
+    expect(count)
+        .toBe(5)
+})
+
